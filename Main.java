d2314a28ade29e26dc89922c8ec36e9edceab125
Shayne Mujuru <shaynemujuru@gmail.com>
2019-02-16 02:14:18 -0700

    Initial commit before refactors

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        Family house1 = new Family();
+        double[] h1 = house1.makeFamily(30, 45000, 2500, 50, 150, 32, 62000, 4500, 25, 75, 0.15);
+
+        Family house2 = new Family();
+        double[] h2 = house1.makeFamily(42, 77000, 1700, 20, 110, 32, 70000, 1000, 25, 75, 0.2);
+
+        Family house3 = new Family();
+        double[] h3 = house1.makeFamily(21, 38000, 1900, 50, 150, 27, 87000, 2700, 55, 28, 0.30);
+
+        //Greatest house income 
+        double max = 0;
+        if (h1[0] > 0 && h1[0] >= h2[0]) 
+            max = h1[0];
+        else 
+            max = h2[0];
+            
+        if (h3[0] > max) 
+            max = h3[0];  
+            
+        System.out.println("Greatest income: " + max);
+
+        //Greatest income after exp
+        max = 0;
+        if (h1[1] > 0 && h1[1] >= h2[1]) 
+            max = h1[1];
+        else 
+            max = h2[1];
+            
+        if (h3[1] > max) 
+            max = h3[1];     
+        
+        System.out.println("After savings: " + max);
+
+        //Greatest savings
+        max = 0;
+        if (h1[2] > 0 && h1[2] >= h2[2]) 
+            max = h1[2];
+        else 
+            max = h2[2];
+            
+        if (h3[2] > max) 
+            max = h3[2]; 
+        
+        System.out.println("savings: " + max);
+    }
+}
---
+++ Mother.java
@@ -0,0 +1,19 @@
+public class Mother {
+
+    public int age;
+    public double base;
+    public double amount;
+    public double exp;
+    public double exp2; 
+    public double exp3;
+
+    public Mother (int age, int base ) {
+        this.age = age;
+        this.base = base;
+    }
+
+    public double AmountReturned() {
+        amount = base + age + (age * 0.15) - exp - exp2 - exp3 ;
+        return amount;
+    }
+}
